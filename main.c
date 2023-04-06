08f5a8b4251e4906783d02113821b9ef60374512
Maciej Kaczkowski <Maciej.Kaczkowski.stud@pw.edu.pl>
2023-04-06 20:54:43 +0200

    update main -> testing file

--- main.c
+++ main.c
@@ -2,44 +2,24 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/maxdescendants.h>
-
-
-#include <stdio.h>
-#include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 
-void spawn_processes(int depth, int *process_count) {
-    if (depth > 20 || *process_count >= 50) {
-        return;
-    }
 
-    pid_t pid = fork();
+void spawn_process_line() {
 
-    if (pid == 0) { // Child process
-        printf("Process %d created, depth: %d\n", getpid(), depth);
-        (*process_count)++;
-        spawn_processes(depth + 1, process_count);
-
-        if (*process_count < 50) {
-            pid_t sibling_pid = fork();
-            if (sibling_pid == 0) { // Sibling process
-                printf("Process %d created, depth: %d\n", getpid(), depth);
-                (*process_count)++;
-                spawn_processes(depth + 1, process_count);
-            } else if (sibling_pid > 0) {
-                wait(NULL);
-            } else {
-                perror("fork");
-            }
-        }
-
-        _exit(0);
-    } else if (pid > 0) { // Parent process
-        wait(NULL);
-    } else {
-        perror("fork");
-    }
+	int pid;
+	
+	for (int i=0; i<19; i++) {
+		pid = fork();
+		if (pid == 0) {
+			// child process (NO FORK INSIDE THIS ELSE IF!)
+		}
+		else { // parent process
+			wait(NULL);
+			exit(0);
+		}
+	}
 }
 
 int main(int argc, char *argv[])
@@ -52,28 +32,12 @@ int main(int argc, char *argv[])
 
     int min = atoi(argv[1]);
     int max = atoi(argv[2]);
+	int process_count = 1; // Including the initial process
+
+    spawn_process_line();
 
 	printf("Search min: %d\n", min);
 	printf("Search max: %d\n", max);
-
-	// for(int i = 0; i < 10; ++i)
-	// {	
-	// 	pid_t pid;
-	// 	pid = fork();
-	// 	if( pid == 0)
-	// 	{
-	// 		printf("In child => Own pid : %d\n", getpid());
-    //     	printf("In child => Parent's pid : %d\n", getppid());
-	// 		sleep(5);
-	// 		return 0;
-	// 	}
-	// 	else{
-    //   		printf("In Parent => Child's pid is %d\n", pid);
-	// 	    printf("In Parent => Own pid : %d\n", getpid());
-    // }
-	// }
-	int process_count = 1; // Including the initial process
-    spawn_processes(1, &process_count);
 
 	pid_t whoMaxDescendants = 0;
 	int maxDescendants1 = 0;
